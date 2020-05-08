0c75026a218d992d5ab29b67e88536a2bf1d72cf
Rachit Malik <rachitmalik1947@gmail.com>
2020-05-08 16:41:17 +0530

    added files

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Rachit Malik on 2020-05-06.
+//  Copyright © 2020 Rachit Malik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
